7046feb3e12abebea91b12e46f486d0ef0a107ed
Chuanyang Jin <2179418226@qq.com>
2023-04-08 22:28:35 -0400

    update

---
+++ Fine-tuning BERT for Sentiment Analysis/utils.py
@@ -0,0 +1,124 @@
+import datasets
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from torch.utils.data import DataLoader
+from transformers import AutoModelForSequenceClassification
+from torch.optim import AdamW
+from transformers import get_scheduler
+import torch
+from tqdm.auto import tqdm
+import evaluate
+import random
+import argparse
+import nltk
+from nltk.corpus import wordnet
+from nltk import word_tokenize
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+
+random.seed(0)
+
+
+def example_transform(example):
+    
+    example["text"] = example["text"].lower()
+    return example
+
+
+### Rough guidelines --- typos
+# For typos, you can try to simulate nearest keys on the QWERTY keyboard for some of the letter (e.g. vowels)
+# You can randomly select each word with some fixed probability, and replace random letters in that word with one of the 
+# nearest keys on the keyboard. You can vary the random probablity or which letters to use to achieve the desired accuracy.
+
+
+### Rough guidelines --- synonym replacement
+# For synonyms, use can rely on wordnet (already imported here). Wordnet (https://www.nltk.org/howto/wordnet.html) includes
+# something called synsets (which stands for synonymous words) and for each of them, lemmas() should give you a possible synonym word.
+# You can randomly select each word with some fixed probability to replace by a synonym.
+
+
+
+def custom_transform(example):
+    
+    ################################
+    ##### YOUR CODE BEGINGS HERE ###
+    
+    # Design and implement the transformation as mentioned in pdf
+    # You are free to implement any transformation but the comments at the top roughly describe
+    # how you could implement two of them --- synonym replacement and typos.
+    
+    # You should update example["text"] using your transformation
+
+    typo_prob=0.1
+    synonym_prob=0.5
+
+    # Typos
+    qwerty_mapping = {
+        "a": "qwsz",
+        "b": "vghn",
+        "c": "xdfv",
+        "d": "erfcxs",
+        "e": "wrsd",
+        "f": "rtgvcd",
+        "g": "tyhbvf",
+        "h": "yujnbg",
+        "i": "ujko",
+        "j": "uikmnh",
+        "k": "iolmj",
+        "l": "opk",
+        "m": "njk",
+        "n": "bhjm",
+        "o": "iklp",
+        "p": "ol",
+        "q": "wa",
+        "r": "edft",
+        "s": "awedxz",
+        "t": "rfgy",
+        "u": "yhji",
+        "v": "cfgb",
+        "w": "qase",
+        "x": "zsdc",
+        "y": "tghu",
+        "z": "asx"
+    }
+    
+    def generate_typos(word):
+        if len(word) <= 1:
+            return word
+        typo_word = ""
+        for i, char in enumerate(word):
+            if random.random() < typo_prob:
+                if char in qwerty_mapping:
+                    candidates = qwerty_mapping[char]
+                    typo_char = random.choice(candidates)
+                else:
+                    typo_char = char
+                typo_word += typo_char
+            else:
+                typo_word += char
+        return typo_word
+    
+    example["text"] = " ".join([generate_typos(word) if random.random() < typo_prob else word for word in example["text"].split()])
+    
+    nltk.data.path.append("/home/cj2133/nltk_data")
+    nltk.download("wordnet")
+                  
+    # Synonym replacement
+    def generate_synonym(word):
+        synsets = wordnet.synsets(word)
+        if len(synsets) == 0:
+            return word
+        synonyms = set()
+        for syn in synsets:
+            for lemma in syn.lemmas():
+                synonyms.add(lemma.name())
+        synonyms.discard(word)
+        if len(synonyms) == 0:
+            return word
+        return random.choice(list(synonyms))
+    
+    example["text"] = " ".join([generate_synonym(word) if random.random() < synonym_prob else word for word in example["text"].split()])
+    
+    
+    ##### YOUR CODE ENDS HERE ######
+    
+    return example
